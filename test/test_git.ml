c6b2bb5abd1f86364539304ffeb680a3b0b0722b
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-12-06 15:11:32 +0100

    Test module Git with clone_repo

--- test/test_git.ml
+++ test/test_git.ml
@@ -5,6 +5,10 @@ let pp_repo fmt (Repo x) = Format.fprintf fmt "Report %s" x
 
 let eq_repo = Alcotest.of_pp pp_repo
 
+let pp_hash fmt (Hash x) = Format.fprintf fmt "Hash %s" x
+
+let eq_hash = Alcotest.of_pp pp_hash
+
 let name_project repo =
   String.split_on_char '/' repo |> fun l ->
   let n = List.length l in
@@ -32,6 +36,39 @@ let open_and_clean () =
   let actual = Sys.file_exists !pwd in
   Alcotest.(check bool) "with_tmp_clone cleans the repo" false actual
 
+(** Gitlab project for testing purposes
+
+    https://gitlab.com/vch9/ometrics-test *)
+let git = "git@gitlab.com:vch9/ometrics-test.git"
+
+let no_merge_commit () =
+  let branch = "no-commits" in
+  let repo = clone_repository ~branch git in
+  let actual = find_last_merge_commit repo in
+  Alcotest.(check (option eq_hash)) "no_merge_commit returns None" None actual
+
+let find_merge_commit () =
+  let branch = "merge-commits" in
+  let repo = clone_repository ~branch git in
+  let expected = Some (Hash "7b3d5b8c1c054d280637f4e11cd97135577d3fb5") in
+  let actual = find_last_merge_commit repo in
+  Alcotest.(check (option eq_hash))
+    "find_merge_commit should find" expected actual
+
+let get_commits_after () =
+  let branch = "merge-commits" in
+  let repo = clone_repository ~branch git in
+  let expected =
+    [
+      Hash "ca65467249118435c2ecf4d784c121c0456ffbdc";
+      Hash "73ef14ae0ef1277df03688f8ce7cb96b9a151e5a";
+    ]
+  in
+  let hash = find_last_merge_commit repo |> Option.get in
+  let actual = get_commits_after repo hash in
+  Alcotest.(check (list eq_hash))
+    "get_commits_after gets [foo;bar]" expected actual
+
 let tests =
   ( "Git",
     Alcotest.
@@ -39,4 +76,7 @@ let tests =
         test_case "open_current_repo" `Quick open_current_repo;
         test_case "open_bad_repo" `Quick open_bad_repo;
         test_case "open_and_clean" `Quick open_and_clean;
+        test_case "no_merge_commit" `Quick no_merge_commit;
+        test_case "find_merge_commit" `Quick find_merge_commit;
+        test_case "get_commits_after" `Quick get_commits_after;
       ] )
