25179f9a9a17b00703fc1712e5ebcf077738e790
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-12-21 17:40:21 +0100

    Create clickable link for entries

--- lib/interface.ml
+++ lib/interface.ml
@@ -3,25 +3,29 @@ open Misc
 open Typedtree
 open Types
 
-let entries_of_sig_value ns comments ident descr =
+let entries_of_sig_value ~path ns comments ident descr =
   [
     {
       entry_name = fully_qualified_name ns ident;
       entry_kind = Value;
       entry_documented = is_documented comments descr.val_loc;
+      entry_line = line descr.val_loc;
+      entry_file = path;
     };
   ]
 
-let entries_of_sig_type ns comments ident decl =
+let entries_of_sig_type ~path ns comments ident decl =
   [
     {
       entry_name = fully_qualified_name ns ident;
       entry_kind = Type;
       entry_documented = is_documented comments decl.type_loc;
+      entry_line = line decl.type_loc;
+      entry_file = path;
     };
   ]
 
-let rec entries_of_sig_module ns comments ident decl =
+let rec entries_of_sig_module ~path ns comments ident decl =
   (* {
    *   entry_name = fully_qualified_name ns ident;
    *   entry_kind =
@@ -29,25 +33,25 @@ let rec entries_of_sig_module ns comments ident decl =
    *   entry_documented = is_documented comments decl.md_loc;
    * }
    * :: *)
-  entries_of_module_type ns comments ident decl.md_type
+  entries_of_module_type ~path ns comments ident decl.md_type
 
-and entries_of_module_type ns comments ident = function
+and entries_of_module_type ~path ns comments ident = function
   | Mty_signature signature ->
       List.concat_map
-        (entries_of_sig_item (Ident.name ident :: ns) comments)
+        (entries_of_sig_item ~path (Ident.name ident :: ns) comments)
         signature
-  | Mty_functor (_, mtyp) -> entries_of_module_type ns comments ident mtyp
+  | Mty_functor (_, mtyp) -> entries_of_module_type ~path ns comments ident mtyp
   | _ -> []
 
-and entries_of_sig_item ns comments : Types.signature_item -> entry list =
+and entries_of_sig_item ~path ns comments : Types.signature_item -> entry list =
   function
   | Sig_value (ident, descr, Exported) ->
-      entries_of_sig_value ns comments ident descr
+      entries_of_sig_value ~path ns comments ident descr
   | Sig_type (ident, decl, _, Exported) ->
-      entries_of_sig_type ns comments ident decl
+      entries_of_sig_type ~path ns comments ident decl
   | Sig_module (ident, _, decl, _, Exported) ->
-      entries_of_sig_module ns comments ident decl
+      entries_of_sig_module ~path ns comments ident decl
   | _ -> []
 
-let to_entries ns comments i =
-  List.concat_map (entries_of_sig_item ns comments) i.sig_type
+let to_entries ~path ns comments i =
+  List.concat_map (entries_of_sig_item ~path ns comments) i.sig_type
