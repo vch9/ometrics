d7853344fdea0619fc417637f25840c70510e1d3
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2022-01-10 16:42:54 +0100

    Update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,35 +2,32 @@
 
 set -e
 
-mrs=$(curl -X GET "https://gitlab.com/api/v4/projects/3836952/merge_requests?state=all")
+mrs=$(curl --silent -X GET "https://gitlab.com/api/v4/projects/3836952/merge_requests?state=opened")
 
 i=0
 
 check_mr() {
-	if [ -z "$1" ]; then
-		echo "Missing git repository, example:"
-		echo "$0 https://gitlab.com/vch9/ometrics-test.git"
-		exit 1
-	fi
-
-	if [ ! -z "$2" ]; then
-		branch_option=" --branch $2 "
-	fi
-
-	if [ ! -z "$3" ]; then
-		hash_option=" --hash $3 "
-	fi
-
-	echo "Running: ometrics check-clone $1 $branch_option $hash_option"
-	ometrics check-clone "$1 $branch_option $hash_option"
+	ometrics check-clone $1 \
+			   --html \
+			   --clickable \
+			   --exclude-re "test_*" \
+			   --exclude "tezt/" \
+			   --branch "$2" \
+			   --title "$3"	\
+			   --output "$5"
 }
 
-while true; do
+for i in {0..99}; do
 	mr=$(echo $mrs | jq ".[${i}]")
+	id=$(echo $mr | jq '.id')
 	source_branch=$(echo $mr | jq '.source_branch')
 	trimmed_source=$(echo $source_branch | sed -r 's/[\"]//g')
-	log_file="$trimmed_source.log"
-	check_mr "https://gitlab.com/nomadic-labs/tezos.git" $source_branch > $log_file 2>&1
+	source_project=$(echo $mr | jq '.source_project_id')
+	http_url_to_project=$(curl --silent -X GET "https://gitlab.com/api/v4/projects/${source_project}" | jq '.http_url_to_repo')
+	title=$(echo $mr | jq '.title')
 
-	i=$i+1
+	mkdir -p $1
+	output="$1/$id.html"
+
+	check_mr $http_url_to_project $source_branch "${title}" "$id" $output
 done
