4253600026de4d7408cd915cabdf9ac84eeaee99
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2022-04-22 16:15:36 +0200

    Test: remove dead code

--- test/test_implementation.ml
+++
@@ -1,8 +0,0 @@
-module Implem = Ometrics__Implementation
-
-let () =
-  let open Implem in
-  let _ = to_entries in
-  ()
-
-let tests = ("Implementation", [])
--- test/test_interface.ml
+++
@@ -1,8 +0,0 @@
-module Interface = Ometrics__Interface
-
-let () =
-  let open Interface in
-  let _ = to_entries in
-  ()
-
-let tests = ("Interface", [])
